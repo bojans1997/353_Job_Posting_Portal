29ab9531bb5c920f318368f5c5aa884dd8f3b8e3
DESKTOP-3IMGBC4\Bojan Srbinoski <b2thes@hotmail.com>
2020-08-06 14:30:30 -0400

    push fix to delete job applications before job when deleting a job

--- php_scripts/delete_job.php
+++ php_scripts/delete_job.php
@@ -4,6 +4,9 @@ session_start();
 
 require($_SERVER["DOCUMENT_ROOT"]."/353_Main_Project/php_scripts/connect.php");
 
+$deleteApplications = $conn->prepare("DELETE FROM job_application where job_id = ?");
+$deleteApplications->execute([$_GET["jobID"]]);
+
 $deleteJob = $conn->prepare("DELETE FROM job where id = ?");
 $deleteJob->execute([$_GET["jobID"]]);
 
